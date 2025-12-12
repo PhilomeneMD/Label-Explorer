98079d2aefcf1df34d7452e3c7bc1e9e2b9d7e36
PhilomeneMD <philomene.mojondoyard@psl.eu>
2025-12-12 12:00:52 +0100

    Update REDIRECT_URI based on hostname

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 // src/config.js
 export const SPOTIFY_CLIENT_ID = "c5034b1aceac404e92d9f62f37dd7228";
-export const REDIRECT_URI = "http://127.0.0.1:3000/callback";
+
+const isLocalhost =
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost";
+
+export const REDIRECT_URI = isLocalhost
+  ? "http://127.0.0.1:3000/callback"
+  : "https://labelexplorer.netlify.app/callback";
+
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 
